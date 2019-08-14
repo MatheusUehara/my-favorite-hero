a05744e6a9610083cda9c1c38b8ef94c813b9fc4
Daivid Vasconcelos leal <daivid.leal.trd@c6bank.com>
2019-08-14 20:15:47 -0300

    refactor hero adapter

--- app/src/main/kotlin/com/br/myfavoritehero/features/listCharacter/HeroAdapter.kt
+++ app/src/main/kotlin/com/br/myfavoritehero/features/listCharacter/HeroAdapter.kt
@@ -3,7 +3,6 @@ package com.br.myfavoritehero.features.listCharacter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.br.myfavoritehero.R
 import com.br.myfavoritehero.data.interfaces.HeroEventListener
@@ -13,74 +12,53 @@ import com.br.myfavoritehero.util.getLargeLandscapeThumbnail
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.hero_item.view.favoriteIcon
-import kotlinx.android.synthetic.main.hero_item.view.heroName
 import kotlinx.android.synthetic.main.hero_item.view.heroCardImage
+import kotlinx.android.synthetic.main.hero_item.view.heroName
 
 class HeroAdapter(
     private val elements: ArrayList<Hero>,
     private val listener: HeroEventListener
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    companion object {
-        private const val HERO_TYPE = 0
-        private const val LOADING_TYPE = 1
-    }
-
     private var isLoading = false
     private var context: Context? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == HERO_TYPE) {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.hero_item, parent, false)
-            context = view.context
-            HeroViewHolder(view)
-        } else {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.hero_list_loading, parent, false)
-            context = view.context
-            HeroLoadingViewHolder(view)
-        }
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.hero_item, parent, false)
+        context = view.context
+        return HeroViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        var favorited: Boolean = false
-        if (holder is HeroViewHolder) {
-            val hero = elements[position]
-            Picasso.get().load(hero.thumbnail.path.getLargeLandscapeThumbnail()).into(holder.mLinearLayout.heroCardImage)
-            holder.mLinearLayout.heroName.text = hero.name
-            holder.mLinearLayout.setOnClickListener { listener.onHeroClicked(hero) }
-            context?.let {
-                favorited = SharedPreferencesHelper.isFavorited(it, hero.id)
-                switchVisibility(favorited, holder)
-            }
+        var favorited = false
+        holder as HeroViewHolder
+        val hero = elements[position]
+        Picasso.get().load(hero.thumbnail.path.getLargeLandscapeThumbnail()).into(holder.mLinearLayout.heroCardImage)
+        holder.mLinearLayout.heroName.text = hero.name
+        holder.mLinearLayout.setOnClickListener { listener.onHeroClicked(hero) }
+        context?.let {
+            favorited = SharedPreferencesHelper.isFavorited(it, hero.id)
+            switchVisibility(favorited, holder)
+        }
 
-            holder.mLinearLayout.favoriteIcon.setOnClickListener {
-                favorited = if (favorited) {
-                    SharedPreferencesHelper.setFavorite(it.context, hero.id, false)
-                    Snackbar.make(holder.mLinearLayout, R.string.unFavorited, Snackbar.LENGTH_SHORT).show()
-                    !favorited
-                } else {
-                    holder.mLinearLayout.favoriteIcon.setImageResource(R.drawable.un_favorite)
-                    SharedPreferencesHelper.setFavorite(it.context, hero.id)
-                    Snackbar.make(holder.mLinearLayout, R.string.favorited, Snackbar.LENGTH_SHORT).show()
-                    !favorited
-                }
-                switchVisibility(favorited, holder)
+        holder.mLinearLayout.favoriteIcon.setOnClickListener {
+            favorited = if (favorited) {
+                SharedPreferencesHelper.setFavorite(it.context, hero.id, false)
+                Snackbar.make(holder.mLinearLayout, R.string.unFavorited, Snackbar.LENGTH_SHORT).show()
+                !favorited
+            } else {
+                holder.mLinearLayout.favoriteIcon.setImageResource(R.drawable.un_favorite)
+                SharedPreferencesHelper.setFavorite(it.context, hero.id)
+                Snackbar.make(holder.mLinearLayout, R.string.favorited, Snackbar.LENGTH_SHORT).show()
+                !favorited
             }
-        } else if (holder is HeroLoadingViewHolder) {
-            holder.mShimmerFrameLayout.startShimmer()
-            context?.let {
-                holder.mLoadingHeroRecyclerView.layoutManager = LinearLayoutManager(it)
-            }
-            holder.mLoadingHeroRecyclerView.adapter = HeroLoadingAdapter()
+            switchVisibility(favorited, holder)
         }
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (position >= elements.size) LOADING_TYPE else HERO_TYPE
-    }
-
     override fun getItemCount(): Int {
-        return if (isLoading) elements.size + 1 else elements.size
+        //return if (isLoading) elements.size + 1 else elements.size
+        return elements.size
     }
 
     fun startLoading() {
